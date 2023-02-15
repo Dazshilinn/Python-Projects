b2199c29cba6a5098ae7511bfa5401aca53e8886
Alexander Dexter <ALEXANDER.DEXTER@studytafensw.edu.au>
2023-02-15 14:30:53 +1100

    Initial commit

---
+++ Week 02/Condtional_statements_1.py
@@ -0,0 +1,14 @@
+number = 9
+if number > 10:
+    print("the number is bigger than 10")
+else:
+    print("the number is less than 10")
+
+if 10 < 5:
+    print("False, statement skipped")
+if 0 < 5:
+    print("true, block executed")
+if 0 < 3:
+    print("true,block will also be execute")
+else:
+    print("If all fails.")
