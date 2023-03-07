1740e14c6cd04b6075d99a08014463b43a6eef7c
Alexander Dexter <alexdexter1984@gmail.com>
2023-03-07 17:00:25 +1100

    practice 01.py

---
+++ Practice/practice 01.py
@@ -0,0 +1,9 @@
+valid_username = "alex".upper()
+
+entered_username = input("Enter a valid username ")
+
+if entered_username == valid_username:
+    print("valid username accepted logging you in... ")
+
+else:
+    print("non-valid username")
