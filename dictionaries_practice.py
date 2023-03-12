327e41937648b5b9e85773397632e0f0b29823cb
Alexander Dexter <alexdexter1984@gmail.com>
2023-03-12 20:37:32 +1100

    for tuesday

---
+++ dictionaries_practice.py
@@ -0,0 +1,10 @@
+my_dictionary = {"author": "brandon sanderson", "book": "The Way of Kings":}
+
+keys = my_dictionary()
+print(keys)
+
+values = my_dictionary.values()
+print()
+
+items = my_dictionary()
+print()
